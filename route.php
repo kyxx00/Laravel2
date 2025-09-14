36c1e9fc7fdf21ecfc9c01af66f5a4192f48e98b
kyxx <68198922+kyxx00@users.noreply.github.com>
2025-09-14 22:25:13 +0200

    Add files via upload

---
+++ route.php
@@ -0,0 +1,13 @@
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+ 
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+Route::get('/planeten', function () {
+    return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+});
+ 
+?>
