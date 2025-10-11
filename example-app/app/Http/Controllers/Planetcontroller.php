049176c03f7c2d976db4c87d96946836ec434b79
mootje750 <68198922+mootje750@users.noreply.github.com>
2025-10-11 14:10:17 +0200

    artisan laravel opdracht

---
+++ example-app/app/Http/Controllers/Planetcontroller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlanetController extends Controller
+{
+    // Private array met alle planeten
+    private $planets = [
+        'mars' => [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        'venus' => [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        'earth' => [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
+        ],
+        'jupiter' => [
+            'name' => 'Jupiter',
+            'description' => "Jupiter is a gas giant and doesn't have a solid surface, but it may have a solid inner core about the size of Earth."
+        ],
+    ];
+
+
+    public function index()
+    {
+        return view('planets', ['planets' => $this->planets]);
+    }
+
+
+    public function show($planet)
+    {
+        $planet = strtolower($planet);
+
+        if (!array_key_exists($planet, $this->planets)) {
+            abort(404, 'Planet not found');
+        }
+
+        $selectedPlanet = $this->planets[$planet];
+        return view('planet-detail', ['planet' => $selectedPlanet]);
+    }
+}
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,39 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-$planets = [
-    'mars' => [
-        'name' => 'Mars',
-        'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-    ],
-    'venus' => [
-        'name' => 'Venus',
-        'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-    ],
-    'earth' => [
-        'name' => 'Earth',
-        'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
-    ],
-    'jupiter' => [
-        'name' => 'Jupiter',
-        'description' => "Jupiter is a gas giant and doesn't have a solid surface, but it may have a solid inner core about the size of Earth."
-    ],
-];
+use App\Http\Controllers\PlanetController;
 
 
-Route::get('/planets', function () use ($planets) {
-    return view('planets', ['planets' => $planets]);
-});
+Route::get('/planets', [PlanetController::class, 'index']);
+Route::get('/planets/{planet}', [PlanetController::class, 'show']);
 
 
-Route::get('/planets/{planet}', function ($planet) use ($planets) {
-    $planet = strtolower($planet);
-
-    if (!array_key_exists($planet, $planets)) {
-        abort(404, 'Planet not found');
-    }
-
-    $selectedPlanet = $planets[$planet];
-    return view('planets', ['planets' => [$selectedPlanet]]);
+Route::get('/', function () {
+    return view('welcome');
 });
